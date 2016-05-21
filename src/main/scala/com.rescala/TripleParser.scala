421b4ff62ea48c9575736df4e49ea9a982d6e210
jpmfribeiro <mjoao@student.ethz.ch>
2016-05-21 12:13:00 +0200

    Implements TripleParser for reading triples from a file.

---
+++ src/main/scala/com.rescala/TripleParser.scala
@@ -0,0 +1,27 @@
+package com.rescala
+
+import java.io.File
+
+/**
+  * Created by jpedro on 21.05.16.
+  */
+object TripleParser {
+
+  // Parses a text file containing for each line a triple as in: SUBJECT[delim]PREDICATE[delim]OBJECT
+  // [delim] represents the delimiter used and specified as @delim.
+  def parse(filePath: String, delim: String = "\t"): Array[(String, String, String)] = {
+    require(filePath.nonEmpty, "File path cannot be empty.")
+    require(new File(filePath).exists(), "File '" + filePath + "' does not exist.")
+
+    val tripleLines = io.Source.fromFile(filePath).getLines().toArray
+    val triples = tripleLines.map(l => {
+      val splitted = l.split(delim)
+      assume(splitted.size == 3, "Line '" + l + "' does not contain a triple. Check the delimiter used.")
+
+      (splitted(0), splitted(1), splitted(2))
+    })
+
+    return triples
+  }
+
+}
