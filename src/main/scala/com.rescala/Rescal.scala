710c3fbd8233d6d5ff8155eef8fd5f9fe8e6319d
jpmfribeiro <mjoao@student.ethz.ch>
2016-05-21 12:13:57 +0200

    Skeleton for Rescal

--- src/main/scala/com.rescala/Rescal.scala
+++ src/main/scala/com.rescala/Rescal.scala
@@ -5,4 +5,19 @@ package com.rescala
   */
 class Rescal {
 
+  // FIELDS
+
+
+  // PUBLIC INTERFACE
+
+  def fit(triples: Array[(String, String, String)], params: RescalParameters): Unit = {
+
+  }
+
+  def predict(t: (String, String, String)): Double = {
+    0.0
+  }
+
 }
+
+case class RescalParameters()
